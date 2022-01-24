b2d9e0ad597a3873b0c792de656b9bd5c80d315a
Mitterpach <patrik@mitterpach.com>
2022-01-24 16:26:04 +0100

    Fix incorrect range for the board rows

--- drawSudoku.py
+++ drawSudoku.py
@@ -9,7 +9,7 @@ def printBoard( board ):
         print(f'\033[1m{boardChars[i]}\033[0m', end=" ")
     print(" ")
     for i in range(9):
-        print(f'\033[1m{i}\033[0m', end=" ")
+        print(f'\033[1m{i+1}\033[0m', end=" ")
         for ii in range(8):
             print(board[i][ii], end = " ")
         print(board[i][8])
