4bb53e6f5ce6dc304548e0c4921bc0cf0eb06724
Mitterpach <patrik@mitterpach.com>
2022-01-24 18:23:31 +0100

    Refine board printing

--- drawSudoku.py
+++ drawSudoku.py
@@ -4,12 +4,14 @@ def printBoard( board ):
 
     boardChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
-    print(" ", end=" ")
+    print(" \N{cherry blossom} | ", end="")
     for i in range(9):
         print(f'\033[1m{boardChars[i]}\033[0m', end=" ")
     print(" ")
+    print(" ---+-------------------")
     for i in range(9):
-        print(f'\033[1m{i+1}\033[0m', end=" ")
+        print(f'\033[1m  {i+1}\033[0m', end=" | ")
         for ii in range(8):
             print(board[i][ii], end = " ")
         print(board[i][8])
+    print(" ")
