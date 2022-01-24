9c961a68be9853380237c177bc6aa08f244178d2
Mitterpach <patrik@mitterpach.com>
2022-01-24 16:29:32 +0100

    Fix incorrect coords for input entry

--- sudoku.py
+++ sudoku.py
@@ -20,15 +20,15 @@ if __name__ == '__main__':
     while (gameActive) :
         system('clear')
         printBoard(currBoard)
-        print("Enter next move: [> COLUMN ROW NUMBER]")
+        print("Enter next move: [ > COLUMN ROW NUMBER]")
         newCoords = input("> ")
         coordsList = [char for char in newCoords]
         
-        targetColumn = columnCords.index(coordsList[1].upper())
-        targetRow =    coordsList[0]
+        targetColumn = columnCords.index(coordsList[0].upper())
+        targetRow =    coordsList[1]
         targetNumber = coordsList[2]
 
-        currBoard[int(targetRow)][int(targetColumn)] = targetNumber
+        currBoard[int(targetRow)-1][int(targetColumn)] = targetNumber
 
 
 
