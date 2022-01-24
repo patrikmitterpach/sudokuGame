8edc7485d385802e7c86f4be9ec328ee71dc6e64
Mitterpach <patrik@mitterpach.com>
2022-01-24 16:19:00 +0100

    Rename validateSudoku.py

--- validateSudoku.py
+++ validateSudoku.py
@@ -6,7 +6,7 @@
 #   each line contains digits 1-9
 #   each 3x3  contains digits 1-9
 
-def validSolution(board):
+def validateSolution(board):
     digitList = [1,2,3,4,5,6,7,8,9]
 
     # Row validation
